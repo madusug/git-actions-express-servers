c149ecb4dbbd180d0a1305a8ab28419a6aa52597
“madusug” <jamesmadusha@gmail.com>
2025-02-17 20:21:19 -0500

    changes to express.js

--- express.js
+++ express.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => \{
+app.get('/', (req, res) => {
   res.send('Hello World!');
-\});
+});
 
-app.listen(port, () => \{
-  console.log(`App listening at http://localhost:$\{port\}`);
-\});
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
 
